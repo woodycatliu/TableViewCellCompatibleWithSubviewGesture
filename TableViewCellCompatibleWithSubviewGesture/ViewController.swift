b62d4e125595c53eb1034838b4f925301bbcbff7
woodycatliu <bestlifeforzhi@gmail.com>
2021-04-26 00:02:18 +0800

    Initial Commit

---
+++ TableViewCellCompatibleWithSubviewGesture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewCellCompatibleWithSubviewGesture
+//
+//  Created by Woody Liu on 2021/4/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
